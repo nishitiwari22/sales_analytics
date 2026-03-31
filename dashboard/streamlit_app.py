00b3fd8bb699db1f41a9033b8775eb794c2bd409
Nishi Tiwari <90550117+nishitiwari22@users.noreply.github.com>
2026-03-31 23:33:23 +0530

    Update streamlit_app.py

--- dashboard/streamlit_app.py
+++ dashboard/streamlit_app.py
@@ -1,28 +1,41 @@
+# import streamlit as st
+# import pandas as pd
+# # from sqlalchemy import create_engine
+
+# # engine = create_engine("sqlite:///sales.db")
+
+# st.title("📊 Sales Analytics Dashboard")
+
+# # df = pd.read_sql("SELECT * FROM sales", engine)
+
+# st.subheader("Raw Data")
+# st.dataframe(df)
+# # 
+# # Revenue by Region
+# st.subheader("Revenue by Region")
+# region_sales = df.groupby("Region")["Sales"].sum()
+# st.bar_chart(region_sales)
+
+# # Profit by Category
+# st.subheader("Profit by Category")
+# category_profit = df.groupby("Category")["Profit"].sum()
+# st.bar_chart(category_profit)
+
+# @st.cache_data
+# def load_data():
+#     return pd.read_sql("SELECT * FROM sales", engine)
+
+# df = load_data()
+
+
 import streamlit as st
 import pandas as pd
-# from sqlalchemy import create_engine
-
-# engine = create_engine("sqlite:///sales.db")
-
-st.title("📊 Sales Analytics Dashboard")
-
-# df = pd.read_sql("SELECT * FROM sales", engine)
-
-st.subheader("Raw Data")
-st.dataframe(df)
-
-# Revenue by Region
-st.subheader("Revenue by Region")
-region_sales = df.groupby("Region")["Sales"].sum()
-st.bar_chart(region_sales)
-
-# Profit by Category
-st.subheader("Profit by Category")
-category_profit = df.groupby("Category")["Profit"].sum()
-st.bar_chart(category_profit)
 
 @st.cache_data
 def load_data():
-    return pd.read_sql("SELECT * FROM sales", engine)
+    df = pd.read_csv("data/sales.csv")  # or your file path
+    return df
 
 df = load_data()
+
+st.dataframe(df)
